7dd74a7571ab504ab2a5802a4112b19572781cda
SHEMA Olivier <114829255+obscode20726@users.noreply.github.com>
2023-04-15 00:16:58 +0200

    3rd commit

--- components/Banner.js
+++ components/Banner.js
@@ -2,10 +2,11 @@ import Image from "next/image"
 function Banner() {
   return (
     <div className="relative h-[300px] sm:h-[500px] xl:h-[500px]">
-      <Image src="https://links.papareact.com/0fm" layout="fill" objectFit="cover"  />
+      <Image src="https://links.papareact.com/0fm" layout="fill" objectFit="cover" alt="Banner image" />
+
       <div className="absolute top-1/2 w-full text-center">
         <p className="text-sm sm:text-lg">Not sure where to Go? Perfect.</p>
-        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover: shadow-xl active:scale-90 transition duration-150"> I'm flexible</button>
+        <button className="text-purple-500 bg-white px-10 py-4 shadow-md rounded-full font-bold my-3 hover: shadow-xl active:scale-90 transition duration-150"> I&apos;m flexible</button>
       </div>
     </div>
   )
